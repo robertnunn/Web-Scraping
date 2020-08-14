5848f0bc078fbc08d16e47f23d3207a0d4912886
Rob <robnunn10@gmail.com>
2020-08-14 17:41:10 -0400

    added a scraper to email GTA Online bonuses

---
+++ gta_bonuses.py
@@ -0,0 +1,70 @@
+import os
+from bs4 import BeautifulSoup as BS
+import requests
+import re
+from pprint import pprint as pp
+from email.mime.text import MIMEText
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='D:/scripts/gta.log', filemode='a', format='%(asctime)s %(message)s')
+
+logging.info('Begin script')
+os.chdir('D:/Programming/Projects/Web-Scraping')
+url = 'http://old.reddit.com/r/gtaonline/'
+url_base = 'http://old.reddit.com'
+target_re = re.compile('weekly_gta_online_bonuses', re.IGNORECASE)
+
+with open('gta_email.txt', 'r') as g:
+    recipients = [i.replace('\n', '') for i in g.readlines()]
+
+
+def send_smtp_gmail(email_to, subject, msg="Default message.", login_info='email.json'):
+    """
+    sends an email with the relevant fields using GMail
+    :param email_to: can be a single string, can be a list of strings. Must all be valid email addresses
+    :param subject: subject field of the email
+    :param msg: body text, can be HTML or plain text
+    :param login_info: account credentials for sending email
+    :return: no return value
+    """
+    import smtplib
+    import json
+
+    with open(login_info, 'r') as f:
+        login = json.loads(f.read())
+
+    username = login["username"]
+    password = login["password"]
+    smtp_server = "smtp.gmail.com:587"
+    email_body = ''.join(['From: ', username, '\nSubject: ', subject, "\n", msg])
+
+    server = smtplib.SMTP(smtp_server)
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(username, email_to, email_body)
+
+    server.quit()
+
+
+logging.info('finished setup')
+page = requests.get(url, headers={'User-agent': 'bot-tastic'})
+# pp(page.headers)
+page.raise_for_status()
+soup = BS(page.text, 'lxml')
+logging.info('retrieved and parsed r/gtaonline')
+link = soup.find('a', href=target_re)
+print(bonus_url := url_base + link.get('href'))
+
+page = requests.get(bonus_url, headers={'User-agent': 'only scraping once per week'})
+page.raise_for_status()
+soup = BS(page.text, 'lxml')
+logging.info('retrieved and parsed bonus page')
+post = soup.select('div[class~=expando] > form > div > div[class=md]')[0]
+date = soup.find('title').string
+date = date[:date.find(' ')]
+subj = 'GTA Online Bonuses for ' + date
+msg_body = '<html><body>Here are the GTA Online bonuses for ' + date + ':</br></br>' + str(post)
+mesg = MIMEText(msg_body, 'html').as_string()
+logging.info('email body composed')
+send_smtp_gmail(recipients, subj, mesg)
+logging.info('script complete')
--- scratch.py
+++ scratch.py
@@ -3,29 +3,122 @@ import requests
 import os
 import re
 from pprint import pprint as pp
+import pandas as pd
 
-tests = ['Ceramic x2 Plastic x2',
-         'Aluminum Resin x2', 
-         'Titanium x2 Steel Aluminum Alloy', 
-         'Rubber x2 Aluminum Alloy x2', 
-         'Aluminum Alloy x3', 
-         'Aluminum x2', 
-         'Explosive Powder Aluminum x2 Steel',
-         'Aluminum Aluminum Alloy x2',
-         'Aluminum Alloy Aluminum x2',
-         ]
+def process_ing_list(ing_list, patterns):
+    ing_dict = dict()
+    # print(ing_list)
+    for pattern in patterns:
+        count = pattern.findall(ing_list)
+        if len(count) == 0:
+            pass
+                # print(f'No match found: {ing_list}')
+        else:
+            print(f'Found {len(count)} matches...')
+            for i in count:
+                if i[1] == '':
+                    num = 1
+                else:
+                    num = int(i[1])
+                ing_dict[i[0]] = num
+                print(f'Matched: {i} in "{ing_list}". rebuilt: {i[0]} x{str(num)}')
 
-alre = re.compile('(Aluminum(?: Alloy)?)(?: x(\d))?')
+    return ing_dict
 
-for test in tests:
-    count = alre.findall(test)
-    if len(count) == 0:
-        print(f'No match found: {test}')
-    else:
-        print(f'Found {len(count)} matches...')
-        for i in count:
-            if i[1] == '':
-                num = 1
-            else:
-                num = int(i[1])
-            print(f'Matched: {i} in "{test}". rebuilt: {i[0]} x{str(num)}')
+def process_info_box(url):
+    item_page = requests.get(url)  # get the page
+    item_page.raise_for_status()  # check for errors
+    soup = bs4.BeautifulSoup(item_page.text, 'lxml')  # soupify!
+    
+    info_box = soup.find('table', class_='infoboxtable')  # there should be only one table with a "class" attr that is exactly 'infoboxtable'
+    # info_box = soup.find('table', attrs={'class': 'infoboxtable'})  # alternate way of finding the same table
+    rows = info_box.find_all('tr')  # grab all the rows out of the table
+    row_strings = list()
+    info = dict()
+    # remap_dict is for internal use only
+    remap_dict = {'Tier': 'size',
+                  'Group': 'group',
+                  'Type': 'type',
+                  'Crafted at': 'crafted_at',
+                  'Recipe': 'recipe',
+                  'Unlock Cost': 'research_cost'}
+    for i in rows:  # strip line breaks and commas out (as this will ultimately be a csv)
+        row_strings.append([j.replace('\n', '').replace(',', '') for j in i.strings])
+    
+    parsed_table = pd.read_html(str(soup.select('table[class~="recipeTable"]')[0]))
+    recipe_dict = process_ing_list(parsed_table[0]['Input'][0], patterns)
+    
+    for i in row_strings:  # this for loop iterates through each row of the table and extracts data under the assumption that "key" appears before "value" _somewhere_
+        attr = ''
+        # print(i)
+        while(len(i)):  # because we're popping list items, the len(list) will be 0 when finished with that row
+            term = i.pop(0)
+            # print(f'term: {term}')
+            if term in remap_dict.keys():  # is this data that we're looking for?
+               attr = term
+            elif len(term) > 3 and term != 'Bytes':  # special case for the research_cost 
+                try:
+                    info[remap_dict[attr]] = term
+                except KeyError:
+                    print(f'Infobox error: attr={attr}, term={term}')
+            
+            # print(info)
+    info['research_cost'] = info.get('research_cost', 'Unlocked').strip()  # data validation for the cost and some formatting
+    print(info)
+    info['recipe'] = recipe_dict
+
+    return info
+
+
+# Tuple of search patterns
+base_patterns = ('(Ammonium)',
+                '(Argon)',
+                '(Carbon)',
+                '(Ceramic)',
+                '(Clay)',
+                '(Compound)',
+                '(Copper)',
+                '(Diamond)',
+                '(Explosive Powder)',
+                '(Glass)',
+                '(Graphene)',
+                '(Graphite)',
+                '(Helium)',
+                '(Hematite)',
+                '(Hydrazine)',
+                '(Hydrogen)',
+                '(Iron)',
+                '(Laterite)',
+                '(Lithium)',
+                '(Malachite)',
+                '(Methane)',
+                '(Nanocarbon Alloy)',
+                '(Nitrogen)',
+                '(Organic)',
+                '(Plastic)',
+                '(Quartz)',
+                '(Resin)',
+                '(Rubber)',
+                '(Silicone)',
+                '(Sphalerite)',
+                '(Steel)',
+                '(Sulfur)',
+                '(Titanite)',
+                '(Wolframite)',
+                '(Zinc)',
+                '(Aluminum(?: Alloy)?)',
+                '(Titanium(?: Alloy)?)',
+                '(Tungsten(?: Carbide)?)',
+)
+digit_group = '(?: x(\d))?'
+# original order: i+digit_group
+patterns = [re.compile(i+digit_group) for i in base_patterns]  # append the digit group and compile the regexes
+
+urls =  [
+        'https://astroneer.gamepedia.com/Large_Rover_Seat',
+        'https://astroneer.gamepedia.com/Large_Shredder',
+
+        ]
+
+for _ in urls:
+    pp(process_info_box(_))
